36ca91d50724beebe10d1f0c01a0bd486b16fa71
Mujahid2000 <developermujahid2001@gmail.com>
2025-04-10 16:05:35 +0600

    try to solve some mistake

--- Store/Store.ts
+++ Store/Store.ts
@@ -1,6 +1,6 @@
 
 // lib/store.js
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import authReducer from '@/Store/authSlice';
 
 export const store = configureStore({
@@ -11,3 +11,9 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
