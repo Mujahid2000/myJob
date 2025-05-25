42eecd30bee531ef6fcd5d619de77fec5ef713fc
Mujahid2000 <developermujahid2001@gmail.com>
2025-05-25 20:36:20 +0600

    add last try params type try to solve

--- app/company-dashboard/my-jobs/[id]/page.tsx
+++ app/company-dashboard/my-jobs/[id]/page.tsx
@@ -1,20 +1,15 @@
 import ApplicationDetails from '@/Component/Employee-Dashboard/MyJobComponent/ApplicationDetails';
 import React from 'react';
 
-// Define PageProps to match Next.js expectations
-interface PageProps {
-  params: { id: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
-}
 
-const Page = async ({ params }: PageProps): Promise<React.JSX.Element> => {
-  const { id } = params;
+export default async function page ({ params }: {params: { id: string }}) {
+    const { id } = params;
 
-  return (
-    <div>
-      <ApplicationDetails jobId={id} />
-    </div>
-  );
+    return (
+        <div>
+            <ApplicationDetails jobId={id} />
+        </div>
+    );
 };
 
-export default Page;
+
