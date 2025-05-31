ced1ec9b860ffee63fc33368c1264da70c17f3d9
Mujahid2000 <developermujahid2001@gmail.com>
2025-06-01 03:58:17 +0600

    add main link

--- Component/signleJob/JobDetails.tsx
+++ Component/signleJob/JobDetails.tsx
@@ -89,7 +89,7 @@ return (
                 <Card className=" rounded-none shadow-none border-none">
                     <div className="flex flex-col lg:flex-row items-start gap-3 lg:gap-0 lg:items-center justify-between px-5 lg:px-0">
                         <div className="flex flex-col lg:flex-row items-start lg:items-center gap-4">
-                            <img src={singlePost?.logo} alt="Instagram" className="w-[5rem] lg:w-[6rem] h-[5rem] lg:h-[6rem] rounded-full" />
+                            <img src={singlePost?.logo} alt="Instagram" className="w-[5rem] lg:w-[6rem] h-[5rem] lg:h-[6rem] lg:rounded-full" />
                             <div>
                                 <div className='flex flex-row items-center gap-3'>
                                     <div>
